eb21f9a156e678eb6fb53331e42755b80aa2c3eb
英苹 <53040521+gdds-ads@users.noreply.github.com>
2024-03-27 14:15:06 +0800

    dag

--- dag.go
+++ dag.go
@@ -1,6 +1,20 @@
 package merkledag
 
-import "hash"
+import (
+	"encoding/json"
+	"fmt"
+	"hash"
+	"math"
+)
+
+const (
+	KB          = 1 << 10
+	ChunkSize   = 256 * KB
+	MaxListLine = 4096
+	BLOB        = "blob"
+	LINK        = "link"
+	TREE        = "tree"
+)
 
 type Link struct {
 	Name string
@@ -9,11 +23,163 @@ type Link struct {
 }
 
 type Object struct {
-	Links []Link
+	Links []*Link // 使用指针切片存储Link
 	Data  []byte
 }
 
 func Add(store KVStore, node Node, h hash.Hash) []byte {
 	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	return nil
+	var obj Object
+	switch node.Type() {
+	case FILE:
+		obj = *handleFile(node, store, h) // 解引用指针获取实际对象
+		break
+	case DIR:
+		obj = *handleDir(node, store, h) // 解引用指针获取实际对象
+		break
+	}
+	JsonObj, _ := json.Marshal(obj)
+	return computeHash(obj, JsonObj, h)
+}
+
+func handleFile(node Node, store KVStore, h hash.Hash) *Object {
+	obj := &Object{} // 使用指针类型的Object
+	FileNode, _ := node.(File)
+	if FileNode.Size() > ChunkSize {
+		numChunks := math.Ceil(float64(FileNode.Size()) / float64(ChunkSize))
+		height := 0
+		tmp := numChunks
+		for {
+			height++
+			tmp /= MaxListLine
+			if tmp == 0 {
+				break
+			}
+		}
+		obj, _ = dfsHandleFile(height, FileNode, store, 0, h) // 直接返回指针
+	} else {
+		obj.Data = FileNode.Bytes()
+		putObjInStore(obj, store, h)
+	}
+	return obj
+}
+
+func handleDir(node Node, store KVStore, h hash.Hash) *Object {
+	dirNode, _ := node.(Dir)
+	iter := dirNode.It()
+	treeObject := &Object{} // 使用指针类型的Object
+	for iter.Next() {
+		node := iter.Node()
+		switch node.Type() {
+		case FILE:
+			file := node.(File)
+			tmp := handleFile(node, store, h)
+			jsonMarshal, _ := json.Marshal(tmp)
+			treeObject.Links = append(treeObject.Links, &Link{ // 存储Link指针
+				Hash: computeHash(*tmp, jsonMarshal, h),
+				Size: int(file.Size()),
+				Name: file.Name(),
+			})
+			if tmp.Links == nil {
+				treeObject.Data = append(treeObject.Data, []byte(BLOB)...)
+			} else {
+				treeObject.Data = append(treeObject.Data, []byte(LINK)...)
+			}
+
+			break
+		case DIR:
+			dir := node.(Dir)
+			tmp := handleDir(node, store, h)
+			jsonMarshal, _ := json.Marshal(tmp)
+			treeObject.Links = append(treeObject.Links, &Link{ // 存储Link指针
+				Hash: computeHash(*tmp, jsonMarshal, h),
+				Size: int(dir.Size()),
+				Name: dir.Name(),
+			})
+			treeObject.Data = append(treeObject.Data, []byte(TREE)...)
+			break
+		}
+	}
+	putObjInStore(treeObject, store, h)
+	return treeObject
+}
+
+func computeHash(obj Object, data []byte, h hash.Hash) []byte {
+	if len(obj.Links) == 0 {
+		h.Reset()
+		h.Write(data)
+	} else {
+		hash := make([]byte, 0)
+		for i := 0; i < len(obj.Links); i++ {
+			h.Reset()
+			hash = append(hash, obj.Links[i].Hash...)
+			h.Write(hash)
+		}
+	}
+
+	return h.Sum(nil)
+}
+
+func dfsHandleFile(height int, node File, store KVStore, start int, h hash.Hash) (*Object, int) {
+	obj := &Object{} // 使用指针类型的Object
+	lenData := 0
+	// 处理多层分片
+	for i := 1; i <= MaxListLine && start < len(node.Bytes()); i++ {
+		var tmpObj *Object // 使用指针类型的Object
+		var tmpDataLen int
+
+		if height > 1 {
+			// 递归处理下一层数据
+			tmpObj, tmpDataLen = dfsHandleFile(height-1, node, store, start, h)
+		} else {
+			// 处理当前层数据
+			end := start + ChunkSize
+			if end > len(node.Bytes()) {
+				end = len(node.Bytes())
+			}
+			data := node.Bytes()[start:end]
+			// 将数据存储到 KVStore
+			blobObj := &Object{ // 使用指针类型的Object
+				Links: nil,
+				Data:  data,
+			}
+			putObjInStore(blobObj, store, h)
+			// 更新 obj 中的 Links 和 Data
+			jsonMarshal, _ := json.Marshal(blobObj)
+			obj.Links = append(obj.Links, &Link{ // 存储Link指针
+				Hash: computeHash(*blobObj, jsonMarshal, h),
+				Size: len(data),
+			})
+
+			obj.Data = append(obj.Data, []byte(BLOB)...)
+			tmpDataLen = len(data)
+			start += ChunkSize
+		}
+
+		lenData += tmpDataLen
+		jsonMarshal, _ := json.Marshal(tmpObj)
+		obj.Links = append(obj.Links, &Link{ // 存储Link指针
+			Hash: computeHash(*tmpObj, jsonMarshal, h),
+			Size: tmpDataLen,
+		})
+		obj.Data = append(obj.Data, []byte(LINK)...)
+
+		if start >= len(node.Bytes()) {
+			break
+		}
+	}
+
+	// 将处理好的对象存储到 KVStore
+	putObjInStore(obj, store, h)
+	return obj, lenData
+}
+
+func putObjInStore(obj *Object, store KVStore, h hash.Hash) {
+	value, err := json.Marshal(obj)
+	if err != nil {
+		fmt.Println("json.Marshal err:", err)
+		return
+	}
+	hash := computeHash(*obj, value, h)
+	store.Put(hash, value)
 }
--- dag2file.go
+++ dag2file.go
@@ -1,7 +1,73 @@
 package merkledag
 
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
 // Hash to file
+
 func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
 	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
-	return nil
+	var treeObj Object
+	treeObj = *getObjectByHash(store, hash)
+	obj := getNode(store, path, hp, treeObj)
+	var data []byte
+	//分区数据处理
+	if obj.Links != nil {
+		data = obj.Data
+	} else {
+		getDfsData(store, obj, data)
+	}
+	return data
+}
+func getDfsData(store KVStore, object Object, data []byte) []byte {
+	obj := &Object{}
+	for i := 0; i < len(object.Links); i++ {
+		obj = getObjectByHash(store, object.Links[i].Hash)
+		if obj.Links != nil {
+			getDfsData(store, *obj, data)
+		} else {
+			data = append(data, obj.Data...)
+		}
+	}
+	return data
+}
+func getNode(store KVStore, path string, hp HashPool, object Object) Object {
+	for _, part := range splitPath(path) {
+		var dirHash []byte
+		for i := 0; i < len(object.Links); i++ {
+			if object.Links[i].Name == part {
+				dirHash = object.Links[i].Hash
+				break
+			}
+		}
+		//获取子节点object
+		jsonObj, _ := store.Get(dirHash)
+		var obj Object
+		err := json.Unmarshal(jsonObj, &obj)
+		if err != nil {
+			fmt.Println("解析字符串错误")
+		} else {
+			object = obj
+		}
+	}
+
+	return object
+}
+
+func splitPath(path string) []string {
+	//获取子节点Name长度
+	return strings.Split(path, "/")
+}
+
+func getObjectByHash(store KVStore, hash []byte) *Object {
+	obj := &Object{}
+	jsonTreeObj, _ := store.Get(hash)
+	err := json.Unmarshal(jsonTreeObj, &obj)
+	if err != nil {
+		fmt.Println("解析字符串错误")
+	}
+	return obj
 }
